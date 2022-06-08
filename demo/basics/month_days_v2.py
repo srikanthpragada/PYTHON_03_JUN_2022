cd214d7bf9705e0ce003369b08b8287d8f7ef425
srikanthpragada <srikanthpragada@gmail.com>
2022-06-08 19:58:34 +0530

    08-JUN

---
+++ demo/basics/month_days_v2.py
@@ -0,0 +1,12 @@
+month = int(input("Enter month :"))
+
+if month == 2:
+    year = int(input("Enter year :"))
+    if year % 4 == 0:  # leap year
+        print(29)
+    else:
+        print(28)
+elif month in [4, 6, 9, 11]:
+    print(30)
+else:
+    print(31)
---
+++ demo/basics/while_demo.py
@@ -0,0 +1,4 @@
+n = 1
+while n <= 10:
+    print(n, end=' ')
+    n = n + 1
